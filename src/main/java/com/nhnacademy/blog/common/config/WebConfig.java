40ebf038ff59fc39a26e9c2215f1a596dd298c1d
nhn-academy-marco <marco@ nhnacademy.com>
2025-01-18 09:53:40 +0900

    LoginCheckInterceptor 추가

--- src/main/java/com/nhnacademy/blog/common/config/WebConfig.java
+++ src/main/java/com/nhnacademy/blog/common/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
+ * TODO#5 - WebMvcConfigurer
  * Spring MVC에서 WebMvcConfigurer는 웹 애플리케이션의 MVC 구성 요소를 커스터마이징하기 위한 인터페이스입니다.
  * 이를 구현하면 Spring MVC의 기본 설정을 확장하거나 사용자 정의 로직을 추가할 수 있습니다.
  * WebMvcConfigurer는 Spring Boot와 Spring Framework의 자동 설정을 변경하거나 추가 설정을 적용하는 데 사용됩니다.
@@ -15,9 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        //TODO#6 - registry에 LoginCheckInterceptor등록
         registry
+                //registry에 addInterceptor() 메서드를 이용해서 LoginCheckInterceptor를 추가 합니다.
                 .addInterceptor(new LoginCheckInterceptor())
-                .addPathPatterns("/member/myinfo.do","/member/logout.do")
+
+                //.addPathPatterns() method를 사용해서 /member/myinfo.do, /member/logout.do 지정하세요.
+                // /member/login.do <-- 로그인 form 임으로 로그인한 사용자도 접근할 수 있음으로  개별적으로 지정 합니다.
+                .addPathPatterns("/member/myinfo.do", "/member/logout.do")
+
+                //.excludePathPatterns()는 제외할 uri를 설정할 수 있습니다.
+                // ex) /index.do, /member/login.do
                 .excludePathPatterns("/index.do");
     }
 
--- src/main/java/com/nhnacademy/blog/common/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/nhnacademy/blog/common/interceptor/LoginCheckInterceptor.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Objects;
 
 /**
+ * TODO#1 - interceptor란?
  * Spring에서 Interceptor는 특정 요청이 컨트롤러에 도달하기 전,
  * 또는 응답이 클라이언트로 전달되기 전에 추가적인 로직을 실행할 수 있도록 하는 컴포넌트입니다.
  * Spring MVC에서 제공하는 인터셉터는 주로 **전처리(pre-processing)**와 후처리(post-processing) 작업에 사용됩니다.
@@ -21,6 +22,12 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        /**
+         * TODO#2 - preHandle구현
+         * 요청(Request)이 Controller에 도달하기 전에 실행됩니다
+         * @see com.nhnacademy.blog.common.interceptor.LoginCheckInterceptor 를 참고하여 구현 합니다.
+         *
+         */
 
         HttpSession session = request.getSession(false);
         if(Objects.nonNull(session)) {
@@ -34,16 +41,21 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
             response.sendRedirect("/index.do");
         }
 
+        //true를 반환 합니다. -> 다른 Interceptor가 존재하면 실행 합니다.
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //TODO#3 - Controller가 요청을 처리한 후, View로 데이터를 전달하기 전에 실행됩니다.
+        //로그를 작성해서 동작하는지 확인합니다.
         log.debug("LoginCheckInterceptor:postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //TODO#4 - 요청 처리의 모든 작업(뷰 렌더링 포함)이 완료된 후에 실행됩니다.
+        //로그를 작성해서 동작하는지 확인합니다.
         log.debug("LoginCheckInterceptor:afterCompletion");
     }
 }
