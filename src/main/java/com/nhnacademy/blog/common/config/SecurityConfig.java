bb93d6feba06f7ec1b36b4454e3a250a6787a330
nhn-academy-marco <marco@ nhnacademy.com>
2025-01-24 23:29:20 +0900

    spring-security-username-password

--- src/main/java/com/nhnacademy/blog/common/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/blog/common/config/SecurityConfig.java
@@ -14,7 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration  // 이 클래스가 Spring Security 설정을 위한 구성 클래스임을 나타냅니다.
 @EnableWebSecurity(debug = true)  // Spring Security의 웹 보안을 활성화하고, 디버깅 정보를 출력하도록 설정합니다.
 public class SecurityConfig {
-
+    /**
+     * TODO#2 - '/*' <-- 모든 요청에 대해서 인증받은 사용자만 서비스를 이용할 수 있습니다.
+     *  HttpSecurity를 사용해서 보안 설정을 정의할 수 있습니다.
+     *  - httpBasic 활성화 합니다.
+     *  - formLogin 활성화 합니다.
+     */
     // HttpSecurity 객체를 이용해 보안 설정을 정의하는 메서드
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // HTTP 요청에 대한 보안 설정
@@ -28,17 +33,23 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // UserDetailsService 빈을 정의하여 사용자 인증 정보를 제공합니다.
+    /**
+     * TODO#3 - UserDetailsService 빈을 정의하여 사용자 인증 정보를 제공합니다.
+     * UserDetailsService는 사용자의 인증 정보를 가져오는 역할을 합니다.
+     * Spring Security는 이 인터페이스를 통해 사용자 이름을 기반으로 사용자 정보를 조회하고,
+     * 그 정보를 사용하여 인증을 수행합니다. 기본적으로 아이디, 비밀번호, 권한 등의 정보를 제공합니다.
+     */
+
     @Bean
     public UserDetailsService userDetailsService() {
         // "marco"라는 사용자 정보를 정의하고, 기본 비밀번호 인코더를 사용하여 암호화합니다.
         UserDetails userDetails = User.withDefaultPasswordEncoder()  // 기본 비밀번호 인코더 사용
-                .username("marco")  // 사용자 이름
-                .password("12345")  // 비밀번호
-                .roles("MEMBER")  // 사용자 역할 (MEMBER)
+                .username("marco")  //TODO#3-1 사용자 이름
+                .password("nhnacademy")  //TODO#3-2 비밀번호
+                .roles("MEMBER")  //TODO#3-3 사용자 역할 (MEMBER)
                 .build();  // UserDetails 객체 생성
 
-        // InMemoryUserDetailsManager를 사용하여 메모리 내에서 사용자 정보를 관리합니다.
+        //TODO#3-4 InMemoryUserDetailsManager를 사용하여 메모리 내에서 사용자 정보를 관리합니다.
         return new InMemoryUserDetailsManager(userDetails);
     }
 }
